961678e00d06db5105ca4f9b1ff79f1fd4162ac6
shameer125 <alishameer251@gmail.com>
2026-05-21 21:12:10 -0700

    some changes in server.jsx

--- src/pages/service.jsx
+++ src/pages/service.jsx
@@ -9,6 +9,7 @@ import ScrollToTop from "../components/scroll-to-top";
 import SEO from "../components/seo";
 
 const ServicePage = () => {
+
     return (
         <React.Fragment>
             <Layout>
@@ -30,6 +31,7 @@ const ServicePage = () => {
             </Layout>
         </React.Fragment>
     );
+    
 };
 
 export default ServicePage;
